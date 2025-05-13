3594b5ce79a7571f24951d906dec7905552a216b
Aditya1603200 <adityadhage1603@gmail.com>
2025-05-13 10:55:28 +0530

    Finished One to one mapping

--- src/main/java/com/Driver.java
+++ src/main/java/com/Driver.java
@@ -33,7 +33,9 @@ public class Driver {
 //		et.commit();
 //		findByDate(LocalDate.now());
 		
-		allocateEngine(108,13);
+//		allocateEngine(108,13);
+//		removeEngine(108);
+		findAllCar();
 		
 	}
 	
@@ -49,38 +51,6 @@ public class Driver {
 		li.forEach(al->System.out.println(al.getId()));
 	}
 	
-//	public static Car findCarById(int id) {
-//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgres");
-//		EntityManager em = emf.createEntityManager();
-//		EntityTransaction et = em.getTransaction();	
-//		
-//		String jpql = "select c from Car c where c.id=?1";
-//		Query query  = em.createQuery(jpql);
-//		query.setParameter(1, id);
-//		Car car = (Car)query.getSingleResult();
-//		return car;
-//	}
-//	
-//	public static void removeEnginebyCarId(int id) {
-//		Car car = findCarById(id);
-//		Engine e = new Engine();
-//		e.setId(NULL);
-//		car.setEngine();
-//		
-//		
-//		EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgres");
-//		EntityManager em = emf.createEntityManager();
-//		EntityTransaction et = em.getTransaction();	
-//		
-//		String jpql = "update Car c set c.e_id = NULL where c.id=?1";
-//		Query query  = em.createQuery(jpql);
-//		query.setParameter(1, engineId);
-//		
-//		et.begin();
-//		em.persist(car);
-//		et.commit();
-//	}
-//	
 	public static void allocateEngine(int car_id,int engine_id) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgres");
 		EntityManager em = emf.createEntityManager();
@@ -90,12 +60,42 @@ public class Driver {
 //		String jpql = "Update car c set c.engine=:e_id where c.id=:c_id";
 		String sql = "Update Car set e_id=:e_id where id=:c_id";
 		et.begin();
-		Query query  = em.createQuery(sql);
+//		Query query = em.createQuery(sql);
+		Query query  = em.createNativeQuery(sql);
 		query.setParameter("e_id",engine_id);
 		query.setParameter("c_id",car_id);
 		int count = query.executeUpdate();
 		System.out.println(count);
 		et.commit();
 	}
-
+	public static void deallocateEngine(int car_id) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgres");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();	
+		
+//		Not running because c.engine is object reference cant pass in jpql therefore use native query
+//		String jpql = "Update car c set c.engine=:e_id where c.id=:c_id";
+		String sql = "Update Car set e_id=NULL where id=:c_id";
+		et.begin();
+//		Query query = em.createQuery(sql);
+		Query query  = em.createNativeQuery(sql);
+		query.setParameter("c_id",car_id);
+		int count = query.executeUpdate();
+		System.out.println(count);
+		et.commit();
+	}
+	public static void findAllCar() {
+		//
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgres");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction et = em.getTransaction();
+		
+		et.begin();
+		String jpql = "Select c from Car c";
+		Query query = em.createQuery(jpql);
+		
+		List<Car> list = query.getResultList();
+		list.forEach(al->System.out.println(al));
+		et.commit();
+	}
 }
